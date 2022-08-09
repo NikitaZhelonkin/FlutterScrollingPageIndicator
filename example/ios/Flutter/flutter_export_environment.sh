cc0156dca8dd6fc68a612a514348d4668f92190b
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-08-10 00:44:38 +0300

    ⬆️ chore: update pubspec.lock

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nikitazhelonkin/Work/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nikitazhelonkin/Work/git/scrolling_page_indicator/example"
+export "FLUTTER_ROOT=/Users/inventrohyder/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/inventrohyder/Downloads/FlutterScrollingPageIndicator/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/nikitazhelonkin/Work/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
