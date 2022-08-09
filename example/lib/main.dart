5f5efaadcc89a08f7732b92f97b7b5382a0b8d21
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-08-10 00:57:38 +0300

    ⬆️ chore: upgrade app to null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,8 +17,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -27,7 +26,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  PageController _controller;
+  late PageController _controller;
 
   @override
   void initState() {
